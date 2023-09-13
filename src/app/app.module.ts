b572acb25749e8e5e09d9a1c4478f953cd768ea6
Jhoseph <jhosephazeredo@gmail.com>
2023-09-13 13:04:16 -0300

    componentes criados com estilos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { TitleHomeComponent } from './components/title-home/title-home.component';
+import { GrandCardComponent } from './components/grand-card/grand-card.component';
+import { MinorCardComponent } from './components/minor-card/minor-card.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    TitleHomeComponent,
+    GrandCardComponent,
+    MinorCardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/grand-card/grand-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grand-card',
+  templateUrl: './grand-card.component.html',
+  styleUrls: ['./grand-card.component.css']
+})
+export class GrandCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
